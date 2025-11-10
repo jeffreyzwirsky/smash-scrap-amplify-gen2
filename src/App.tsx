8f525a4b72c034e64a75b8857526a6faf32ee639
jeffreyzwirsky <jeff@smashscrap.ca>
2025-11-10 04:41:44 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,11 @@ import { Dashboard } from './pages/Dashboard'
 import { Organizations } from './pages/Organizations'
 import { Boxes } from './pages/Boxes'
 import { BoxDetails } from './pages/BoxDetails'
+import { Parts } from './pages/Parts'
 import { Sales } from './pages/Sales'
-
+import { SaleDetails } from './pages/SaleDetails'
+import { Marketplace } from './pages/Marketplace'
+import { MarketplaceListing } from './pages/MarketplaceListing'
 
 function App() {
   return (
@@ -19,10 +22,13 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/organizations" element={<Organizations />} />
-              <Route path="/boxes/:boxId" element={<BoxDetails />} />
-<Route path="/sales" element={<Sales />} />
-<Route path="/sales/:saleId" element={<SaleDetails />} />
               <Route path="/boxes" element={<Boxes />} />
+              <Route path="/boxes/:boxId" element={<BoxDetails />} />
+              <Route path="/parts" element={<Parts />} />
+              <Route path="/sales" element={<Sales />} />
+              <Route path="/sales/:saleId" element={<SaleDetails />} />
+              <Route path="/marketplace" element={<Marketplace />} />
+              <Route path="/marketplace/:saleId" element={<MarketplaceListing />} />
               <Route path="*" element={<div>Page not found</div>} />
             </Routes>
           </Layout>
