0146fce246e3622ec14cdb1794a918722da4c79b
jeffreyzwirsky <jeff@smashscrap.ca>
2025-11-10 14:40:23 -0600

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -8,7 +8,7 @@ import { postConfirmation } from './function/post-confirmation/resource'
 /**
  * SMASH SCRAP - Amplify Gen 2 Backend Configuration
  */
-export const backend = defineBackend({
+const backend = defineBackend({
   auth,
   data,
   storage,
@@ -18,8 +18,4 @@ export const backend = defineBackend({
 
 // Grant post-confirmation Lambda access to DynamoDB User table
 const userTable = backend.data.resources.tables['User']
-backend.postConfirmation.resources.lambda.addEnvironment(
-  'USER_TABLE_NAME',
-  userTable.tableName
-)
 userTable.grantReadWriteData(backend.postConfirmation.resources.lambda)
