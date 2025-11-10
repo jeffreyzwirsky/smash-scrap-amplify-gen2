f74b9a7bd050f16d483cb0046de68adc81420981
jeffreyzwirsky <jeff@smashscrap.ca>
2025-11-10 03:03:22 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import { Amplify } from 'aws-amplify'
+import outputs from '../amplify_outputs.json'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Amplify } from 'aws-amplify'
