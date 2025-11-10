b7bc9f7d60bae30f28118f25b86fa80062a4a1fd
jeffreyzwirsky <jeff@smashscrap.ca>
2025-11-10 02:33:58 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { Amplify } from 'aws-amplify'
 import outputs from '../amplify_outputs.json'
-import './index.css'
 import App from './App.tsx'
+import './index.css'
 
-// Configure Amplify with the backend resources
 Amplify.configure(outputs)
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
