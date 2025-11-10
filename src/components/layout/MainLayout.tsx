81429d7009db44cc644cc782b22ec111aa47f044
jeffreyzwirsky <jeff@smashscrap.ca>
2025-11-10 17:20:05 -0600

    Update MainLayout.tsx

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -14,11 +14,11 @@ export const MainLayout: React.FC<MainLayoutProps> = ({
   onSignOut
 }) => {
   return (
-    <div className="flex min-h-screen bg-black">
+    <div className="flex min-h-screen bg-[#0b1437]">
       <Sidebar />
       <div className="flex-1 flex flex-col">
         <TopBar onRefresh={onRefresh} onSignOut={onSignOut} />
-        <main className="flex-1 p-6 overflow-y-auto">
+        <main className="flex-1 p-8 overflow-y-auto">
           {children}
         </main>
       </div>
